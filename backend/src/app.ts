831dacbf796c43f63099f5f5aff3a74c262db0ca
hakeemnazri <mhakeemnazri@gmail.com>
2025-09-17 19:45:47 +0800

    config file

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import { appServer } from "./setupServer";
 import initDatabase from "./setupDatabase";
+import { config } from "./config";
 class Application {
     public init() : void {
+        this.loadConfig();
         initDatabase();
         const app = express();
         const server = new appServer(app);
         server.start();
+    }
+
+    private loadConfig() : void{
+        config.validateConfig();
     }
 }
 
