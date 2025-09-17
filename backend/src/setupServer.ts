2ca2ae5620e6344c8f25dba445e83ea165573278
hakeemnazri <mhakeemnazri@gmail.com>
2025-09-17 18:18:06 +0800

    security middleware & standard middleware

--- backend/src/setupServer.ts
+++ backend/src/setupServer.ts
@@ -1,33 +1,66 @@
 import {
-    Application,
-    json,
-    urlencoded,
-    Response,
-    Request,
-    NextFunction,
-  } from "express";
-  import http from "http";
-  export class appServer {
-    private app: Application;
-  
-    constructor(app: Application) {
-      this.app = app;
-    }
-  
-    public start(): void {
-      this.securityMiddleware(this.app);
-      this.standardMiddleware(this.app);
-      this.routeMiddleware(this.app);
-      this.globalErrorHandler(this.app);
-      this.initServer(this.app);
-    }
-  
-    private securityMiddleware(app: Application): void {}
-    private standardMiddleware(app: Application): void {}
-    private routeMiddleware(app: Application): void {}
-    private globalErrorHandler(app: Application): void {}
-    private initServer(app: Application): void {}
-    private createSocketIO(httpServer: http.Server): void {}
-    private startHttpServer(httpServer: http.Server): void {}
+  Application,
+  json,
+  urlencoded,
+  Response,
+  Request,
+  NextFunction,
+} from "express";
+import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookieSession from "cookie-session";
+import HTTP_STATUS from "http-status-codes";
+import compression from "compression";
+import "express-async-errors";
+
+export class appServer {
+  private app: Application;
+
+  constructor(app: Application) {
+    this.app = app;
+  }
+
+  public start(): void {
+    this.securityMiddleware(this.app);
+    this.standardMiddleware(this.app);
+    this.routeMiddleware(this.app);
+    this.globalErrorHandler(this.app);
+    this.initServer(this.app);
+  }
+
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        secure: false,
+        httpOnly: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: HTTP_STATUS.OK,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
+
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
   }
   
+  private routeMiddleware(app: Application): void {}
+  private globalErrorHandler(app: Application): void {}
+  private initServer(app: Application): void {}
+  private createSocketIO(httpServer: http.Server): void {}
+  private startHttpServer(httpServer: http.Server): void {}
+}
